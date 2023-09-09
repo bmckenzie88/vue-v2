1eb3a72fe3b9ca4f6417b1391dadd9c43c26adcd
bryce <brycelmckenzie@gmail.com>
2023-09-08 21:48:06 -0700

    refactored navbarlink to generate li element instead of inside navbar

--- vue-v2/src/components/Navbar.vue
+++ vue-v2/src/components/Navbar.vue
@@ -3,7 +3,11 @@
     :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']"
   >
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">My Vue</a>
+      <a
+        class="navbar-brand"
+        href="#"
+        >My Vue</a
+      >
       <button
         class="navbar-toggler"
         type="button"
@@ -15,68 +19,77 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul class="navbar-nav me-auto">
-            <li v-for="(page, index) in publishedPages" class="nav-item" :key="index">
-                <navbar-link
-                    :page="page"
-                    :isActive="activePage ===index"
-                    @click.prevent="navLinkClick(index)">
-                    
-                </navbar-link>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <button class="btn btn-primary" @click.prevent="changeTheme()">
-              Toggle
-            </button>
-          </form>
+      <div
+        class="collapse navbar-collapse"
+        id="navbarNavAltMarkup"
+      >
+        <ul class="navbar-nav me-auto">
+
+          <navbar-link
+            v-for="(page, index) in publishedPages"
+            class="nav-item"
+            :key="index"
+            :page="page"
+            ;index="index"
+            :isActive="activePage === index"
+            @click.prevent="navLinkClick(index)"
+          >
+          </navbar-link>
+
+        </ul>
+        <form class="d-flex">
+          <button
+            class="btn btn-primary"
+            @click.prevent="changeTheme()"
+          >
+            Toggle
+          </button>
+        </form>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import NavbarLink from './NavbarLink.vue';
-export default {
-  components: {
-    NavbarLink
-  },
-  created() {
-    this.getThemeSetting();
-  },
-  computed: {
-    publishedPages() {
-      return this.pages.filter(p=>p.published)
-    }
-  },
-  props: ["pages", "activePage", "navLinkClick"],
-  data() {
-    return {
-      theme: "light",
-    };
-  },
-  methods: {
-    changeTheme() {
-      let theme = "light";
-      if (this.theme == "light") {
-        theme = "dark";
-      }
-
-      this.theme = theme;
-      this.storeThemeSetting()
+  import NavbarLink from './NavbarLink.vue';
+  export default {
+    components: {
+      NavbarLink,
     },
-    storeThemeSetting() {
-      localStorage.setItem('theme', this.theme)
+    created() {
+      this.getThemeSetting();
     },
-    getThemeSetting() {
-            let theme = localStorage.getItem('theme')
+    computed: {
+      publishedPages() {
+        return this.pages.filter((p) => p.published);
+      },
+    },
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data() {
+      return {
+        theme: 'light',
+      };
+    },
+    methods: {
+      changeTheme() {
+        let theme = 'light';
+        if (this.theme == 'light') {
+          theme = 'dark';
+        }
 
-            if (theme) {
-              this.theme = theme
-            }
+        this.theme = theme;
+        this.storeThemeSetting();
+      },
+      storeThemeSetting() {
+        localStorage.setItem('theme', this.theme);
+      },
+      getThemeSetting() {
+        let theme = localStorage.getItem('theme');
 
-    }
-  },
-};
+        if (theme) {
+          this.theme = theme;
+        }
+      },
+    },
+  };
 </script>
--- vue-v2/src/components/NavbarLink.vue
+++ vue-v2/src/components/NavbarLink.vue
@@ -1,33 +1,33 @@
 <template>
   <li>
-
     <a
-    class="nav-link"
-    :class="activeClasses"
-    aria-current="page"
-    :href="page.link.url"
-    :title="`This link goes to the \${page.link.text} page`"
-    >{{ page.link.text }}</a
+      class="nav-link"
+      :class="activeClasses"
+      aria-current="page"
+      :href="page.link.url"
+      :title="`This link goes to the \${page.link.text} page`"
+      @click.prevent="$bus.$emit('navbarLinkActivated, index')"
+      >{{ page.link.text }}</a
     >
   </li>
 </template>
 
 <script>
-export default {
-  props: ['page', 'isActive'],
-  computed: {
-    activeClasses() {
-      return {
-        active: this.isActive,
-        emphasize: this.isActive,
-      };
+  export default {
+    props: ['page', 'index', 'isActive'],
+    computed: {
+      activeClasses() {
+        return {
+          active: this.isActive,
+          emphasize: this.isActive,
+        };
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.emphasize {
-  text-decoration: underline !important;
-}
+  .emphasize {
+    text-decoration: underline !important;
+  }
 </style>
