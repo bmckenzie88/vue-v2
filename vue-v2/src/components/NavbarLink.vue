1866623eea850f54d72155981dd94a385164bc42
bryce <brycelmckenzie@gmail.com>
2023-09-08 21:23:26 -0700

    created utils folder in src, created events.js inside utils folder

--- vue-v2/src/components/NavbarLink.vue
+++ vue-v2/src/components/NavbarLink.vue
@@ -1,12 +1,15 @@
 <template>
-  <a
+  <li>
+
+    <a
     class="nav-link"
     :class="activeClasses"
     aria-current="page"
     :href="page.link.url"
     :title="`This link goes to the \${page.link.text} page`"
     >{{ page.link.text }}</a
-  >
+    >
+  </li>
 </template>
 
 <script>
